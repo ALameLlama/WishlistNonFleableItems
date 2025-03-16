fe9d14883bfedca10a13658c333908ee8011c1f5
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2025-03-17 00:17:26 +1100

    feat: start looking only adding items by base type

--- src/mod.ts
+++ src/mod.ts
@@ -7,6 +7,8 @@ import { SaveServer } from "@spt/servers/SaveServer";
 import { WishlistController } from "@spt/controllers/WishlistController";
 import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
 import { ITemplateItem } from "@spt/models/eft/common/tables/ITemplateItem";
+import { ItemBaseClassService } from "@spt/services/ItemBaseClassService";
+import { BaseClasses } from "@spt/models/enums/BaseClasses";
 
 class Mod implements IPostDBLoadMod, IPostSptLoadMod 
 {
@@ -22,6 +24,7 @@ class Mod implements IPostDBLoadMod, IPostSptLoadMod
     {
         const logger = container.resolve<ILogger>("WinstonLogger");
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const itemBaseClassService = container.resolve<ItemBaseClassService>("ItemBaseClassService");
 
         const database = databaseServer.getTables();
 
@@ -33,10 +36,26 @@ class Mod implements IPostDBLoadMod, IPostSptLoadMod
 
         const items: Record<string, ITemplateItem> = database.templates.items;
 
-        // Filter items that can't be sold on the flea market
-        this.nonFleableItems = Object.values(items).filter(item => !item._props.CanSellOnRagfair);
+        const targetBaseClasses = new Set([
+            BaseClasses.AMMO,
+            BaseClasses.KEY
+        ]);
 
-        logger.info(`[${this.mod}] Found ${this.nonFleableItems.length} non-flea items.`);
+        // Filter items that can't be sold on the flea market
+        this.nonFleableItems = Object.values(items).filter(item =>
+        {
+            if (!item._props.CanSellOnRagfair)
+            {
+                // Get the base classes of the item
+                const itemBaseClasses = itemBaseClassService.getItemBaseClasses(item._id);
+
+                // Check if any of the item's base classes match our target set
+                return itemBaseClasses.some(baseClass => targetBaseClasses.has(baseClass as BaseClasses));
+            }
+            return false;
+        });
+
+        logger.info(`[${this.mod}] Found ${this.nonFleableItems.length} non-flea items matching target base classes.`);
     }
 
     public postSptLoad(container: DependencyContainer): void 
